b122c27d7ef1f02c8a327266bb29f0f3f5694c74
Muhammad Bhula <ST10073908@vcconnect.edu.za>
2024-11-21 13:22:41 +0200

    Sonar removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,21 +6,6 @@ plugins {
     // Google services plugin (if you're using Firebase or Google Play services)
     id("com.google.gms.google-services") version "4.4.2" apply false
 
-    // SonarQube plugin
-    id("org.sonarqube") version "6.0.0.5145" apply true
 }
 
-// Configure SonarQube properties
-sonarqube {
-    properties {
-        property("sonar.projectKey", "venem_venem-poultry")
-        property("sonar.organization", "venem")
-        property("sonar.host.url", "https://sonarcloud.io")
 
-        // Additional optional properties
-        property("sonar.language", "kotlin") // Specify language if the project is in Kotlin
-        property("sonar.sources", "src/main/java") // Specify source directory
-        property("sonar.tests", "src/test/java") // Specify test directory
-        property("sonar.java.binaries", "build") // Specify binary output directory
-    }
-}
